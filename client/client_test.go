12ef0ac8247cbc82b8586e4aa9089dc2f798b3df
yenhtt <thanhyen107@gmail.com>
2025-03-23 17:43:53 +0700

    fix: add header to response body

--- client/client_test.go
+++ client/client_test.go
@@ -17,13 +17,13 @@ import (
 var dir, _ = os.Getwd()
 var logDir = fmt.Sprintf("%s/logs", dir)
 var pactDir = fmt.Sprintf("%s/pacts", getParentDirectory())
-var Term = matchers.Term
+var Regex = matchers.Regex
 
 func TestNewV2Pact(t *testing.T) {
 
 	mockProvider, err := consumer.NewV2Pact(consumer.MockHTTPProviderConfig{
-		Consumer: "Sample Book Consumer",
-		Provider: "Sample Book Provider",
+		Consumer: "SampleBookConsumer",
+		Provider: "SampleBookProvider",
 		LogDir:   logDir,
 		PactDir:  pactDir,
 	})
@@ -33,24 +33,23 @@ func TestNewV2Pact(t *testing.T) {
 
 	err = mockProvider.
 		AddInteraction().
-		Given("A product exists").
-		UponReceiving("A request for product").
-		WithRequestPathMatcher(
-			"GET",
-			Term(`^/BookStore/v1/Book/ISBN=[0-9]+$`, "/BookStore/v1/Book/ISBN=9781449331818"),
-		).WillRespondWith(200, func(b *consumer.V2ResponseBuilder) {
-		b.BodyMatch(&Book{
-			ISBN:        "9781449331818",
-			Title:       "Learning JavaScript Design Patterns",
-			SubTitle:    "A JavaScript and jQuery Developer's Guide",
-			Author:      "Addy Osmani",
-			PublishDate: "2020-06-04T09:11:40.000Z",
-			Publisher:   "O'Reilly Media",
-			Pages:       254,
-			Description: "Description",
-			Website:     "hihi",
-		})
-	}).
+		Given("A book exists in system").
+		UponReceiving("A request for book").
+		WithRequest("GET", "/BookStore/v1/Book/ISBN/9781449331818").
+		WillRespondWith(200, func(b *consumer.V2ResponseBuilder) {
+			b.Header("Content-Type", Regex("application/json", "application\\/json"))
+			b.BodyMatch(&Book{
+				ISBN:        "9781449331818",
+				Title:       "Learning JavaScript Design Patterns",
+				SubTitle:    "A JavaScript and jQuery Developer's Guide",
+				Author:      "Addy Osmani",
+				PublishDate: "2020-06-04T09:11:40.000Z",
+				Publisher:   "O'Reilly Media",
+				Pages:       254,
+				Description: "Description",
+				Website:     "hihi",
+			})
+		}).
 		ExecuteTest(t, func(config consumer.MockServerConfig) error {
 
 			fmt.Printf("Pact Mock Server Running at: %s:%d\n", config.Host, config.Port)
@@ -108,7 +107,7 @@ func bookClient(host string, port int) *bookAPIClient {
 
 func (u *bookAPIClient) GetBook(isbn string) (*Book, error) {
 
-	path := fmt.Sprintf("/BookStore/v1/Book/ISBN=%s", isbn)
+	path := fmt.Sprintf("/BookStore/v1/Book/ISBN/%s", isbn)
 	resp, err := http.Get(fmt.Sprintf("http://%s:%d%s", u.host, u.port, path))
 
 	if err != nil {
